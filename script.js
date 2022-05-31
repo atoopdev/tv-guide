16a98b43f316f7b77b949bd4023dbcbc94062c03
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-05-31 14:41:58 -0700

    switch output to all shows matching keyword
    does not return seasons, removed img due to consistent null error. simple css display grid.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 
 let show = "girls"
+let showHTML = ""
 
 async function findShow(search_term){
-   const response = await fetch(`https://api.tvmaze.com/singlesearch/shows?q=${search_term}&embed=seasons`) 
+   const response = await fetch(`https://api.tvmaze.com/search/shows?q=${search_term}`) 
    if(!response.ok){
     const message = `An error has occured: ${response.status}`
     throw new Error(message)
@@ -13,21 +14,28 @@ return data
 }
 
 findShow(show).then(showInfo=>{
-    console.log(showInfo)
-    let showHTML = getShowHTML(showInfo)
+    // console.log(showInfo)
+    // console.log(showInfo[0].show.image.medium)
+    showHTML = showInfo.map(getShowHTML).join('')
     document.getElementById("tvguide").innerHTML = showHTML;
         
 })
 
 function getShowHTML(obj){
+    if(obj.show.summary === null){
+        obj.show.summary = "Summary not found"
+    }
     return `
     <div class = "show">
-    <div class="show-title">${obj.name}</div>
-    <div class="show-img"><img src="${obj.image.medium}"></div>
-    <div class="show-summary">${obj.summary}</div>
-    ${obj._embedded.seasons.map(season =>{
-        return `<div class="show-season"><a class="season-link" href="${season.url}">Season: ${season.number}</a></div>`
-    }).join('')}
+    <div class="show-title">${obj.show.name}</div>
+    <div class="show-summary">${obj.show.summary}</div>
     </div>
     `;
 }
+
+// used to show seasons
+// ${obj._embedded.seasons.map(season =>{
+//     return `<div class="show-season"><a class="season-link" href="${season.url}">Season: ${season.number}</a></div>`
+// }).join('')}
+
+//  <div class="show-img"><img src="${obj.show.image.medium}"></div>
